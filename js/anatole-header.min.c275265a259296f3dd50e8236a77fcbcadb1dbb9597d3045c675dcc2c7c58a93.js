e6485784689af257765536d4a415354dd20cedd6
mjpitz <mjpitz@users.noreply.github.com>
2021-03-20 16:03:14 +0000

    deploy: ebfc55c407f9b48b8f7e1f73ec42f67ef80ef7d1

--- js/anatole-header.min.c275265a259296f3dd50e8236a77fcbcadb1dbb9597d3045c675dcc2c7c58a93.js
+++
@@ -1,9 +0,0 @@
-function getTheme(){return localStorage.getItem('theme')?localStorage.getItem('theme'):null;}
-function setTheme(style){document.documentElement.setAttribute('data-theme',style);localStorage.setItem('theme',style);}
-function init(){var theme=getTheme();const userPrefersDark=window.matchMedia&&window.matchMedia('(prefers-color-scheme: dark)').matches;if(theme===null){if(userPrefersDark){setTheme('dark');}
-else{setTheme('light');}}
-else{if(theme=='light'){document.documentElement.setAttribute('data-theme','light');}
-else{document.documentElement.setAttribute('data-theme','dark');}}}
-function switchTheme(e){var theme=getTheme();if(theme=='light'){setTheme('dark');}
-else{setTheme('light');}}
-document.addEventListener('DOMContentLoaded',function(){var themeSwitcher=document.querySelector('.theme-switch');themeSwitcher.addEventListener('click',switchTheme,false);},false);init();
