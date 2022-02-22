c8d91e51c89401e425c2130d1cc8129f468a8f82
Mya Pitzeruse <j.mya.pitz@gmail.com>
2022-02-21 21:05:29 -0600

    infra: start migrating infrastructure

---
+++ infra/do/admin/main.tf
@@ -0,0 +1,36 @@
+terraform {
+  backend "s3" {
+    skip_credentials_validation = true
+    skip_metadata_api_check = true
+    endpoint = "https://nyc3.digitaloceanspaces.com"
+    region = "us-east-1"
+    bucket = "mya-tfstate"
+    key = "infra/do/admin/terraform.tfstate"
+  }
+}
+
+resource "digitalocean_spaces_bucket" "tfstate" {
+  name = "mya-tfstate"
+  region = "nyc3"
+  acl = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  lifecycle_rule {
+    enabled = true
+
+    expiration {
+      days = 14
+    }
+
+    noncurrent_version_expiration {
+      days = 7
+    }
+  }
+}
+
+resource "digitalocean_project" "mya" {
+  name = "mya"
+}
---
+++ infra/home/README.md
@@ -0,0 +1,3 @@
+# home
+
+My at-home infrastructure is currently managed in my [rpi-cloud-init](https://github.com/mjpitz/rpi-cloud-init) repo.
