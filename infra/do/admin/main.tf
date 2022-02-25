1bf277e36b451847ea81e955fb999563ff163c8f
Mya Pitzeruse <j.mya.pitz@gmail.com>
2022-02-25 08:36:46 -0600

    admin: associate state with project

--- infra/do/admin/main.tf
+++ infra/do/admin/main.tf
@@ -1,18 +1,18 @@
 terraform {
   backend "s3" {
     skip_credentials_validation = true
-    skip_metadata_api_check = true
-    endpoint = "https://nyc3.digitaloceanspaces.com"
-    region = "us-east-1"
-    bucket = "mya-tfstate"
-    key = "infra/do/admin/terraform.tfstate"
+    skip_metadata_api_check     = true
+    endpoint                    = "https://nyc3.digitaloceanspaces.com"
+    region                      = "us-east-1"
+    bucket                      = "mya-tfstate"
+    key                         = "infra/do/admin/terraform.tfstate"
   }
 }
 
 resource "digitalocean_spaces_bucket" "tfstate" {
-  name = "mya-tfstate"
+  name   = "mya-tfstate"
   region = "nyc3"
-  acl = "private"
+  acl    = "private"
 
   versioning {
     enabled = true
@@ -33,4 +33,11 @@ resource "digitalocean_spaces_bucket" "tfstate" {
 
 resource "digitalocean_project" "mya" {
   name = "mya"
+}
+
+resource "digitalocean_project_resources" "resource_allocation" {
+  project   = digitalocean_project.mya.id
+  resources = [
+    digitalocean_spaces_bucket.tfstate.urn,
+  ]
 }
