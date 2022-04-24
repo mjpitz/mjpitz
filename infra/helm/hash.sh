eb3817cdc5598a7dbc62a84cd45ab3e213924128
Mya Pitzeruse <j.mya.pitz@gmail.com>
2022-04-24 15:16:44 -0500

    fix(infra/helm): incorrect hash calculation was causing changes to not be detected

--- infra/helm/hash.sh
+++ infra/helm/hash.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cat <<EOF
 {
-  "sha256": "$(find ${1:-.} -type f -print0 | xargs cat | sha256sum | awk '{print $1}')"
+  "sha256": "$(find ${1:-.} -type f | xargs cat | sha256sum | awk '{print $1}')"
 }
 EOF
