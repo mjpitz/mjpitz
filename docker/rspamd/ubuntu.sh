6a7a291be4508cf2fff9fd17fea349072e4b84d2
Mya Pitzeruse <j.mya.pitz@gmail.com>
2022-04-28 21:51:13 -0500

    fix(docker): generalize container builds

--- docker/rspamd/ubuntu.sh
+++ docker/rspamd/ubuntu.sh
@@ -1,9 +1,9 @@
-apt-get update && apt-get install -y ca-certificates lsb-release wget
+apt-get update && apt-get install -y ca-certificates lsb-release curl gpg
 
 RELEASE_NAME=$(lsb_release -c -s)
 
 mkdir -p /etc/apt/keyrings
-wget -O- https://rspamd.com/apt-stable/gpg.key | gpg --dearmor | tee /etc/apt/keyrings/rspamd.gpg > /dev/null
+curl -sSL https://rspamd.com/apt-stable/gpg.key | gpg --dearmor | tee /etc/apt/keyrings/rspamd.gpg > /dev/null
 echo "deb [arch=amd64 signed-by=/etc/apt/keyrings/rspamd.gpg] http://rspamd.com/apt-stable/ $RELEASE_NAME main" | tee /etc/apt/sources.list.d/rspamd.list
 echo "deb-src [arch=amd64 signed-by=/etc/apt/keyrings/rspamd.gpg] http://rspamd.com/apt-stable/ $RELEASE_NAME main"  | tee -a /etc/apt/sources.list.d/rspamd.list
-apt-get update && apt-get --no-install-recommends install rspamd
+apt-get update && apt-get --no-install-recommends -y install rspamd
