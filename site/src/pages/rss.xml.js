cd7e2ebe2dc9a1021155a5992192c6e5ad4f848f
mya <j.mya.pitz@gmail.com>
2024-08-13 16:22:25 -0500

    feat(site): another round of site edits

--- site/src/pages/rss.xml.js
+++ site/src/pages/rss.xml.js
@@ -2,15 +2,29 @@ import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 import { SITE_TITLE, SITE_DESCRIPTION } from '../consts';
 
-export async function get(context) {
-	const posts = await getCollection('blog');
+const ASCENDING_PUBDATE = (a, b) => a.pubDate.valueOf() - b.pubDate.valueOf();
+
+export async function GET(context) {
+	const [
+		blog,
+		media,
+		papers,
+	] = await Promise.all([
+		getCollection('blog'),
+		getCollection('media'),
+		getCollection('papers'),
+	]);
+
+	let all = [].concat(
+		blog.map((p) => ({...p.data, link: `/blog/${p.slug}/`})),
+		media.map((p) => ({...p.data, link: `/media/${p.slug}/`})),
+		papers.map((p) => ({...p.data, link: `/papers/${p.slug}/`})),
+	);
+
 	return rss({
 		title: SITE_TITLE,
 		description: SITE_DESCRIPTION,
 		site: context.site,
-		items: posts.map((post) => ({
-			...post.data,
-			link: `/blog/${post.slug}/`,
-		})),
+		items: all.sort(ASCENDING_PUBDATE),
 	});
 }
