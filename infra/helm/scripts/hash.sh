f72caabd70eb9bafb8ba8f3cd7ffb6bb17ba0e2e
mya <j.mya.pitz@gmail.com>
2024-09-01 14:39:46 -0500

    feat(infra): enable gitea actions and deprovision woodpecker

--- infra/helm/scripts/hash.sh
+++ infra/helm/scripts/hash.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 cat <<EOF
 {
-  "sha256": "$(find ${1:-.} -type f | xargs -i{} bash -c 'echo "{}"; cat {}' | sha256sum | awk '{print $1}')"
+  "sha256": "$(find ${1:-.} -type f | xargs -I{} bash -c 'echo "{}"; cat {}' | sha256sum | awk '{print $1}')"
 }
 EOF
