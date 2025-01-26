c7c544d697e9fdfae5d46a9b617b4db2c6034de9
mya <j.mya.pitz@gmail.com>
2025-01-26 13:10:48 -0600

    feat: add page on ai usage

--- site/src/pages/robots.txt.js
+++ site/src/pages/robots.txt.js
@@ -1,8 +1,13 @@
+import {SITE_BASE_URL} from "../consts";
+
 const robots = `
 User-agent: *
 Allow: /
+
+Sitemap: ${SITE_BASE_URL}/sitemap-index.xml
 `
 
 export async function GET(context) {
+
     return new Response(robots);
 }
